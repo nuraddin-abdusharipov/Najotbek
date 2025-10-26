dee4f7fa95125672229714292239b8692de0bed3
nuraddin-abdusharipov <breakingnew770@gmail.com>
2025-10-26 19:23:58 +0500

    Remove storage import from firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA_cDEn86AbIjYawA6f-fK_ac9kURjoIIk",
@@ -15,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
