0e5e30f4bec048cda8c28c1dd6a5fb211a9bae16
nuraddin-abdusharipov <breakingnew770@gmail.com>
2025-10-26 18:58:35 +0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
-import { getFunctions } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-functions.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
-// Firebase konfiguratsiyangizni shu yerga qo'ying
 const firebaseConfig = {
   apiKey: "AIzaSyA_cDEn86AbIjYawA6f-fK_ac9kURjoIIk",
   authDomain: "najotbek-57050.firebaseapp.com",
@@ -13,10 +12,7 @@ const firebaseConfig = {
   appId: "1:642212440585:web:8040635fb95d477397a6d8"
 };
 
-// Firebase ni ishga tushirish
 const app = initializeApp(firebaseConfig);
-
-// Authentication va Firestore ni eksport qilish
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const functions = getFunctions(app);
+export const storage = getStorage(app);
--- init-helper.js
+++ init-helper.js
@@ -1,3 +1,2 @@
 import { showNotification } from './notification.js';
-// Example: attach to window for pages to call without import
 window.showNotification = showNotification;
